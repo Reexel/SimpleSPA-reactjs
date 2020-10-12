6d29a54394a2dddfb25b5b20b13ed8084234c7bb
Alexey Shevchenko <reexelmail@gmail.com>
2020-10-12 13:47:39 +0300

    SPA React

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ЕСТЬ ВОПРОСЫ?</h2>
+        <p>
+          Опубликуйте свой вопрос на <a href="htpp://google.com">Google</a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
